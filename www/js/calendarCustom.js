51ee156b6351b179c536d163122e8e06eb9c698d
Prateeksha Singh <singh.prateeksha14@gmail.com>
2017-09-11 16:11:21 +0530

    list Advisor calendar events

--- www/js/calendarCustom.js
+++ www/js/calendarCustom.js
@@ -1,19 +1,37 @@
 document.getElementById("createCalEntry").addEventListener("click",createCalendarEntry);
 
 function createCalendarEntry(){
-					var startDate = new Date(document.getElementById('apptTimeStart').value); 
-					
+					var startDate = new Date(document.getElementById('apptTimeStart').value);
 					var endDate = new Date(document.getElementById('apptTimeEnds').value);
-					
-					var title = "Appointment with Jane Doe";
-					alert("start: "+startDate+" end: "+endDate+" title: "+title+" location: "+eventlocation+" notes: "+notes);
+					var title = "Advisor appointment with Jane Doe";
 					var eventlocation = document.getElementById('location').value;
-					alert("start: "+startDate+" end: "+endDate+" title: "+title+" location: "+eventlocation+" notes: "+notes);
 					var notes = document.getElementById('notes').value;
-					alert("start: "+startDate+" end: "+endDate+" title: "+title+" location: "+eventlocation+" notes: "+notes);
 					var success = function(message) { alert("Success: " + JSON.stringify(message)); };
 					var error = function(message) { alert("Error: " + JSON.stringify(message)); };
 					window.plugins.calendar.createEventInteractively(title,eventlocation,notes,startDate,endDate,success,error);
 					//window.plugins.calendar.openCalendar();
 			}
-			
+		
+document.getElementById("listEvents").addEventListener("click",getListOfEvents);
+
+function getListOfEvents(){
+
+	var startDate = new Date(1990,0,1,0,0,0,0,0);
+	var endDate = new Date(2990,11,31,0,0,0,0,0);
+	var success = function(message) { 
+		alert("Success: " + JSON.stringify(message));
+		var jsonString=JSON.parse(JSON.stringify(message));
+		alert(jsonString.length);
+		var titleArr=[];
+		var j=0;
+		for(i=0;i<jsonString.length;i++){
+			if((jsonString[i].title).includes("Advisor")){
+			titleArr[j]=jsonString[i].title;
+			j++;
+			}
+		}
+		alert(titleArr.toString());
+		};
+	var error = function(message) { alert("Error: " + JSON.stringify(message)); };
+	window.plugins.calendar.listEventsInRange(startDate, endDate,success,error);
+}
